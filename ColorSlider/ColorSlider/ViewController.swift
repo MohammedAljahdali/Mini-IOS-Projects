f057ab238dfaaf80b545fd052396611f30d91970
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-11 21:46:06 +0300

    Created ColorSlider project

---
+++ ColorSlider/ColorSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSlider
+//
+//  Created by Mohammed Khakidaljahdali on 11/09/2019.
+//  Copyright © 2019 Mohammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
