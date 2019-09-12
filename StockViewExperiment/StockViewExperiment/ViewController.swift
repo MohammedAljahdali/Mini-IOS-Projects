4cd1654293b0ee4723e002ec4e85fd9e507bf413
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-12 18:20:15 +0300

    Implemented ImagePicker, Activity and Alert

--- StockViewExperiment/StockViewExperiment/ViewController.swift
+++ StockViewExperiment/StockViewExperiment/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    enum Buttons: Int { case imagePickerView = 0, activityView, alertView}
     @IBAction func experiment(sender: UIButton) {
-        let nextController = UIImagePickerController()
-        present(nextController, animated: true, completion: nil)
+        switch (Buttons(rawValue: sender.tag)!) {
+        case .imagePickerView:
+            let imageController = UIImagePickerController()
+            present(imageController, animated: true, completion: nil)
+        case .activityView:
+            let image = UIImage()
+            let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            present(activityController, animated: true, completion: nil)
+            
+        case .alertView:
+            let alertController = UIAlertController()
+            alertController.title = "Hello"
+            alertController.message = "World"
+            let alertAction = UIAlertAction(title: "Hey", style: UIAlertAction.Style.default) {
+                action in self.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(alertAction)
+            present(alertController, animated: true, completion: nil)
+        }
+        
     }
 
-
+    
 }
 
