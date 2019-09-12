2db8dfd5c8103c4d99b2b0b63e190351cbf9455f
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-12 14:24:50 +0300

    Finshed the Color Slider

--- ColorSlider/ColorSlider/ViewController.swift
+++ ColorSlider/ColorSlider/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeColor()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func changeColor() {
+        var r: Float = redSlider.value
+        var g: Float = greenSlider.value
+        var b: Float = blueSlider.value
+        var redValue: CGFloat = CGFloat(r)
+        var greenValue: CGFloat = CGFloat(g)
+        var blueValue: CGFloat = CGFloat(b)
+        
+        colorView.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1)
+        
     }
 
 
