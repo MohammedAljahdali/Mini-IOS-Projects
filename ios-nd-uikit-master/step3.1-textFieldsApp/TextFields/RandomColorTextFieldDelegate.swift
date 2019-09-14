f53eb9536c990ea4d3ad2de40f93abc6e1144f7f
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-14 21:04:42 +0300

    Finshed the new Delegate, which randomize the color of the text each time you enter a character. completed step3.1

--- ios-nd-uikit-master/step3.1-textFieldsApp/TextFields/RandomColorTextFieldDelegate.swift
+++ ios-nd-uikit-master/step3.1-textFieldsApp/TextFields/RandomColorTextFieldDelegate.swift
@@ -11,13 +11,26 @@ import UIKit
 
 class RandomColorTextFieldDelegate: NSObject, UITextFieldDelegate {
     
-    //    func randomColor() -> UIColor {
-    //        // @TODO: return a random color
-    //    }
+        func randomColor() -> UIColor {
+            // @TODO: return a random color
+            let colors: [UIColor] = [
+            .blue, .black, .cyan, .gray, .orange, .purple, .green
+            ]
+            return colors[Int.random(in: 0..<colors.count)]
+        }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         // @TODO: Set the color of your text here!
+//        var newText = textField.text! as NSString
+//        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        textField.textColor = randomColor()
         return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
     }
     
 }
