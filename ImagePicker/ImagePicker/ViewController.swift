fd250c222b6f6924ec22861a1a76f01cda84b545
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-17 12:17:19 +0300

    Implemented the iamge picker from photo Liabray and camera

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -10,19 +10,35 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet var image: UIView!
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+    }
     @IBAction func imagePicker(_ sender: Any) {
         let controller = UIImagePickerController()
         controller.delegate = self
+        controller.sourceType = .photoLibrary
         present(controller, animated: true, completion: nil)
         
     }
+    @IBAction func cameraImagePicker(_ sender: Any) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = .camera
+        present(controller, animated: true, completion: nil)
+    }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageChosen = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image.image = imageChosen
+            image.contentMode = .scaleToFill
+        }
         dismiss(animated: true, completion: nil)
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
