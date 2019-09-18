63257b345fa4d19ebfe75b21193ad50f8ea124ee
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-09-18 19:43:20 +0300

    Added a textfield on top of image

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    let textAttributes: [NSAttributedString.Key: Any] = [
+        .foregroundColor: UIColor.blue,
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.strokeWidth: 4.0,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+
+    ]
     
-    @IBOutlet weak var image: UIImageView!
+    // MARK: IBOUtlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIButton!
+    
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.defaultTextAttributes = textAttributes
+        textField.text = "Write Here!"
+        textField.textAlignment = NSTextAlignment.center
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(ViewController.upperCaseTextField), for: UIControl.Event.editingChanged)
     }
+    
+    // MARK: viewWillAppear
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
 
     }
+    
+    // MARK: IBActions
     @IBAction func imagePicker(_ sender: Any) {
         let controller = UIImagePickerController()
         controller.delegate = self
@@ -34,16 +53,45 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         controller.sourceType = .camera
         present(controller, animated: true, completion: nil)
     }
+    
+    // MARK: ImagePicker delegate
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let imageChosen = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            image.image = imageChosen
-            image.contentMode = .scaleToFill
+            imageView.image = imageChosen
+            imageView.contentMode = .scaleToFill
         }
         dismiss(animated: true, completion: nil)
     }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: TextField delegate
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        let newText = textField.text! as NSString
+//        newText.replacingCharacters(in: range, with: string.uppercased())
+//        textField.text = newText as String
+//        return true
+////            textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.uppercased())
+////
+////        return true
+//    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "Write Here!" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    @objc func upperCaseTextField() {
+        textField.text = textField.text!.uppercased()
+    }
+    
     
 
 }
